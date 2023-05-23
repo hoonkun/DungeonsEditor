04a75f4493ab64f1e6f74fdeda055aa23c0297f3
hoonkun <herokun.user@gmail.com>
2023-05-23 17:29:38 +0900

    fix: 효과부여 상세화면이 닫히지 않던 문제 수정

--- src/jvmMain/kotlin/composable/popup/EnchantmentDetailView.kt
+++ src/jvmMain/kotlin/composable/popup/EnchantmentDetailView.kt
@@ -29,9 +29,9 @@ fun EnchantmentDetailView(enchantment: Enchantment, requestClose: () -> Unit) {
 
     Row {
         Box(modifier = Modifier.size(300.dp).clickable(source, null) { editorState.detailState.toggleEnchantmentSelector() }) {
-            PopupCloseButton(requestClose)
             EnchantmentIcon(enchantment, scale = 1.0f)
             LevelImagePositioner(size = 0.4f) { LevelImage(enchantment.level, scale = 1.5f) }
+            PopupCloseButton(requestClose)
         }
         Column(modifier = Modifier.padding(top = 20.dp, end = 30.dp, bottom = 30.dp)) {
             Row(verticalAlignment = Alignment.Bottom) {
