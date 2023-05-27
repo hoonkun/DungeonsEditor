7793d90304a2993af2fb0072644337af97a3c984
hoonkun <herokun.user@gmail.com>
2023-05-27 18:48:40 +0900

    checkpoint: ArmorProperty 에 대해 UI 리뉴얼

--- src/jvmMain/kotlin/composable/PopupView.kt
+++ src/jvmMain/kotlin/composable/PopupView.kt
@@ -6,20 +6,20 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.onClick
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import arctic
+import blackstone.states.ArmorProperty
 import blackstone.states.Enchantment
+import composable.blackstone.popup.ArmorPropertyCollection
+import composable.blackstone.popup.ArmorPropertyDetail
 import composable.blackstone.popup.EnchantmentDetail
 import composable.blackstone.popup.EnchantmentsCollection
 
@@ -65,8 +65,35 @@ data class EnchantPopupRightState(
 
 @Composable
 fun BoxScope.ArmorPropertyPopup() {
+    val detailTarget by remember { derivedStateOf { arctic.armorProperties.detailTarget } }
+    val rootEnabled by remember { derivedStateOf { arctic.armorProperties.hasDetailTarget } }
 
+    val detailEnabled by remember { derivedStateOf { arctic.armorProperties.detailTarget.let { it != null } } }
+
+    Backdrop(rootEnabled) { arctic.armorProperties.closeDetail() }
+
+    Row(modifier = Modifier.fillMaxWidth()) {
+        LeftSlide(ArmorPropertyPopupLeftState(rootEnabled, detailTarget)) { (visible, target) ->
+            if (!visible) Box(modifier = Modifier.width(700.dp).fillMaxHeight().align(Alignment.CenterEnd))
+            else ArmorPropertyCollection(target)
+        }
+
+        RightSlide(ArmorPropertyPopupRightState(rootEnabled && detailEnabled, detailTarget)) { (visible, target) ->
+            if (!visible || target == null) Box(modifier = Modifier.size(675.dp, 300.dp))
+            else ArmorPropertyDetail(target)
+        }
+    }
 }
+
+data class ArmorPropertyPopupLeftState(
+    val visible: Boolean,
+    val target: ArmorProperty?
+)
+
+data class ArmorPropertyPopupRightState(
+    val visible: Boolean,
+    val target: ArmorProperty?
+)
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
