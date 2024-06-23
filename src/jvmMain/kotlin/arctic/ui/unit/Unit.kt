0112409d28a350ab31461c52a97d2d4ed0924991
hoonkun <herokun.user@gmail.com>
2024-06-23 18:55:12 +0900

    checkpoint: 레거시 삭제!

--- src/jvmMain/kotlin/arctic/ui/unit/Unit.kt
+++
@@ -1,9 +0,0 @@
-package arctic.ui.unit
-
-val Int.dp get() = this.ddp
-val Float.dp get() = this.ddp
-val Double.dp get() = this.ddp
-
-val Int.sp get() = this.dsp
-val Float.sp get() = this.dsp
-val Double.sp get() = this.dsp
